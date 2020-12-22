057042d280e96a0364377bd9797a3f8361adc9d0
SG <gesongwei666@gmail.com>
2020-12-21 21:11:53 -0500

    aaa

--- run_resnet.sh
+++
@@ -1,5 +0,0 @@
-python cifar10.py --model resnet_18 > /vulcanscratch/songweig/logs/adv_pool/resnet_18.txt
-python cifar10.py --model resnet_34 > /vulcanscratch/songweig/logs/adv_pool/resnet_34.txt
-python cifar10.py --model resnet_50 > /vulcanscratch/songweig/logs/adv_pool/resnet_50.txt
-python cifar10.py --model resnet_101 > /vulcanscratch/songweig/logs/adv_pool/resnet_101.txt
-python cifar10.py --model resnet_152 > /vulcanscratch/songweig/logs/adv_pool/resnet_152.txt
--- run_vgg_no_pooling.sh
+++
@@ -1,3 +0,0 @@
-python cifar10.py --model VGG11_NoPooling > /vulcanscratch/songweig/logs/adv_pool/VGG11_NoPooling.txt
-python cifar10.py --model VGG13_NoPooling > /vulcanscratch/songweig/logs/adv_pool/VGG13_NoPooling.txt
-python cifar10.py --model VGG19_NoPooling > /vulcanscratch/songweig/logs/adv_pool/VGG19_NoPooling.txt
