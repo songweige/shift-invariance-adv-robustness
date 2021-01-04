66b64addc5c26a994a4f90d2ff079c44d892c10d
SG <gesongwei666@gmail.com>
2021-01-04 05:27:58 -0500

    mnist resnet

---
+++ run_resnet_mnist.sh
@@ -0,0 +1,8 @@
+python mnist.py --model resnet_18
+python test_mnist.py --model resnet_18 > /vulcanscratch/songweig/logs/adv_pool/mnist/resnet_18.txt
+python mnist.py --model resnet_max_pooling_18
+python test_mnist.py --model resnet_max_pooling_18 > /vulcanscratch/songweig/logs/adv_pool/mnist/resnet_max_pooling_18.txt
+python mnist.py --model resnet_34
+python test_mnist.py --model resnet_34 > /vulcanscratch/songweig/logs/adv_pool/mnist/resnet_34.txt
+python mnist.py --model resnet_max_pooling_34
+python test_mnist.py --model resnet_max_pooling_34 > /vulcanscratch/songweig/logs/adv_pool/mnist/resnet_max_pooling_34.txt
