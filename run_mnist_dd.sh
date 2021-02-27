842882c439a376f14e974fa0cec79fed74a97d08
SG <gesongwei666@gmail.com>
2021-02-27 02:37:07 -0500

    double descent script

---
+++ run_mnist_dd.sh
@@ -0,0 +1,112 @@
+python mnist.py --model FC --n_hidden 5
+python test_mnist.py --model FC --n_hidden 5 > /vulcanscratch/songweig/logs/adv_pool/FC_5.txt
+python mnist.py --model FC --n_hidden 10
+python test_mnist.py --model FC --n_hidden 10 > /vulcanscratch/songweig/logs/adv_pool/FC_10.txt
+python mnist.py --model FC --n_hidden 50
+python test_mnist.py --model FC --n_hidden 50 > /vulcanscratch/songweig/logs/adv_pool/FC_50.txt
+python mnist.py --model FC --n_hidden 100
+python test_mnist.py --model FC --n_hidden 100 > /vulcanscratch/songweig/logs/adv_pool/FC_100.txt
+python mnist.py --model FC --n_hidden 500
+python test_mnist.py --model FC --n_hidden 500 > /vulcanscratch/songweig/logs/adv_pool/FC_500.txt
+python mnist.py --model FC --n_hidden 1000
+python test_mnist.py --model FC --n_hidden 1000 > /vulcanscratch/songweig/logs/adv_pool/FC_1000.txt
+python mnist.py --model FC --n_hidden 5000
+python test_mnist.py --model FC --n_hidden 5000 > /vulcanscratch/songweig/logs/adv_pool/FC_5000.txt
+python mnist.py --model FC --n_hidden 10000
+python test_mnist.py --model FC --n_hidden 10000 > /vulcanscratch/songweig/logs/adv_pool/FC_10000.txt
+
+
+
+python mnist.py --model FC --n_hidden 550
+python test_mnist.py --model FC --n_hidden 550 > /vulcanscratch/songweig/logs/double_descent/FC_550.txt
+python mnist.py --model FC --n_hidden 600
+python test_mnist.py --model FC --n_hidden 600 > /vulcanscratch/songweig/logs/double_descent/FC_600.txt
+python mnist.py --model FC --n_hidden 650
+python test_mnist.py --model FC --n_hidden 650 > /vulcanscratch/songweig/logs/double_descent/FC_650.txt
+python mnist.py --model FC --n_hidden 700
+python test_mnist.py --model FC --n_hidden 700 > /vulcanscratch/songweig/logs/double_descent/FC_700.txt
+python mnist.py --model FC --n_hidden 750
+python test_mnist.py --model FC --n_hidden 750 > /vulcanscratch/songweig/logs/double_descent/FC_750.txt
+python mnist.py --model FC --n_hidden 800
+python test_mnist.py --model FC --n_hidden 800 > /vulcanscratch/songweig/logs/double_descent/FC_800.txt
+python mnist.py --model FC --n_hidden 850
+python test_mnist.py --model FC --n_hidden 850 > /vulcanscratch/songweig/logs/double_descent/FC_850.txt
+python mnist.py --model FC --n_hidden 900
+python test_mnist.py --model FC --n_hidden 900 > /vulcanscratch/songweig/logs/double_descent/FC_900.txt
+python mnist.py --model FC --n_hidden 950
+python test_mnist.py --model FC --n_hidden 950 > /vulcanscratch/songweig/logs/double_descent/FC_950.txt
+
+
+
+
+
+python mnist.py --model FC --n_hidden 710
+python test_mnist.py --model FC --n_hidden 710 > /vulcanscratch/songweig/logs/double_descent/FC_710.txt
+python mnist.py --model FC --n_hidden 720
+python test_mnist.py --model FC --n_hidden 720 > /vulcanscratch/songweig/logs/double_descent/FC_720.txt
+python mnist.py --model FC --n_hidden 730
+python test_mnist.py --model FC --n_hidden 730 > /vulcanscratch/songweig/logs/double_descent/FC_730.txt
+python mnist.py --model FC --n_hidden 740
+python test_mnist.py --model FC --n_hidden 740 > /vulcanscratch/songweig/logs/double_descent/FC_740.txt
+python mnist.py --model FC --n_hidden 760
+python test_mnist.py --model FC --n_hidden 760 > /vulcanscratch/songweig/logs/double_descent/FC_760.txt
+python mnist.py --model FC --n_hidden 770
+python test_mnist.py --model FC --n_hidden 770 > /vulcanscratch/songweig/logs/double_descent/FC_770.txt
+python mnist.py --model FC --n_hidden 780
+python test_mnist.py --model FC --n_hidden 780 > /vulcanscratch/songweig/logs/double_descent/FC_780.txt
+python mnist.py --model FC --n_hidden 790
+python test_mnist.py --model FC --n_hidden 790 > /vulcanscratch/songweig/logs/double_descent/FC_790.txt
+
+
+
+
+
+python mnist.py --model FC --n_hidden 10
+python test_mnist.py --model FC --n_hidden 10 > /vulcanscratch/songweig/logs/double_descent_4k/FC_10.txt
+python mnist.py --model FC --n_hidden 20
+python test_mnist.py --model FC --n_hidden 20 > /vulcanscratch/songweig/logs/double_descent_4k/FC_20.txt
+python mnist.py --model FC --n_hidden 30
+python test_mnist.py --model FC --n_hidden 30 > /vulcanscratch/songweig/logs/double_descent_4k/FC_30.txt
+python mnist.py --model FC --n_hidden 40
+python test_mnist.py --model FC --n_hidden 40 > /vulcanscratch/songweig/logs/double_descent_4k/FC_40.txt
+python mnist.py --model FC --n_hidden 50
+python test_mnist.py --model FC --n_hidden 50 > /vulcanscratch/songweig/logs/double_descent_4k/FC_50.txt
+python mnist.py --model FC --n_hidden 60
+python test_mnist.py --model FC --n_hidden 60 > /vulcanscratch/songweig/logs/double_descent_4k/FC_60.txt
+python mnist.py --model FC --n_hidden 70
+python test_mnist.py --model FC --n_hidden 70 > /vulcanscratch/songweig/logs/double_descent_4k/FC_70.txt
+python mnist.py --model FC --n_hidden 80
+python test_mnist.py --model FC --n_hidden 80 > /vulcanscratch/songweig/logs/double_descent_4k/FC_80.txt
+python mnist.py --model FC --n_hidden 90
+python test_mnist.py --model FC --n_hidden 90 > /vulcanscratch/songweig/logs/double_descent_4k/FC_90.txt
+python mnist.py --model FC --n_hidden 100
+python test_mnist.py --model FC --n_hidden 100 > /vulcanscratch/songweig/logs/double_descent_4k/FC_100.txt
+
+
+
+python mnist.py --model FC --n_hidden 5
+python test_mnist.py --model FC --n_hidden 5 > /vulcanscratch/songweig/logs/double_descent_4k/FC_5.txt
+python mnist.py --model FC --n_hidden 55
+python test_mnist.py --model FC --n_hidden 55 > /vulcanscratch/songweig/logs/double_descent_4k/FC_55.txt
+python mnist.py --model FC --n_hidden 65
+python test_mnist.py --model FC --n_hidden 65 > /vulcanscratch/songweig/logs/double_descent_4k/FC_65.txt
+python mnist.py --model FC --n_hidden 500
+python test_mnist.py --model FC --n_hidden 500 > /vulcanscratch/songweig/logs/double_descent_4k/FC_500.txt
+python mnist.py --model FC --n_hidden 1000
+python test_mnist.py --model FC --n_hidden 1000 > /vulcanscratch/songweig/logs/double_descent_4k/FC_1000.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
