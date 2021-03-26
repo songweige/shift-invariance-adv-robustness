e0502bf7260f19b053892ebfc8ec89059d9b0d8e
SG <gesongwei666@gmail.com>
2021-03-26 02:02:58 -0400

    script

--- run_mnist_pad.sh
+++ run_mnist_pad.sh
@@ -38,3 +38,24 @@ python mnist.py --model Conv --n_hidden 5000 --padding_size 12
 python test_mnist.py --model Conv --n_hidden 5000 --padding_size 12 > /vulcanscratch/songweig/logs/adv_pool/Conv_5000_padding12.txt
 python mnist.py --model Conv --n_hidden 5000 --padding_size 14
 python test_mnist.py --model Conv --n_hidden 5000 --padding_size 14 > /vulcanscratch/songweig/logs/adv_pool/Conv_5000_padding14.txt
+
+
+python mnist.py --model Conv --n_hidden 500 --padding_size 0
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 0 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding0.txt
+python mnist.py --model Conv --n_hidden 500 --padding_size 2
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 2 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding2.txt
+
+python mnist.py --model Conv --n_hidden 500 --padding_size 12
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 12 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding12.txt
+python mnist.py --model Conv --n_hidden 500 --padding_size 14
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 14 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding14.txt
+
+python mnist.py --model Conv --n_hidden 500 --padding_size 4
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 4 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding4.txt
+python mnist.py --model Conv --n_hidden 500 --padding_size 6
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 6 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding6.txt
+
+python mnist.py --model Conv --n_hidden 500 --padding_size 8
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 8 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding8.txt
+python mnist.py --model Conv --n_hidden 500 --padding_size 10
+python test_mnist.py --model Conv --n_hidden 500 --padding_size 10 > /vulcanscratch/songweig/logs/adv_pool/Conv_500_padding10.txt
